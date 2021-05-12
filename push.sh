cbe3aea849bf110395c9055b24b35b66311c5d59
Thomas Carli <thomas.carli@gmail.com>
2021-05-12 12:46:01 +0200

    circleci config

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@
 
 FIRST_CI_PUSH_STDOUT=`git push origin main:first-ci-passed --porcelain 2>&1`
 FIRST_CI_PUSH_STATUS=$?
-echo $FIRST_CI_PUSH_STDOUT
+echo "==> command exit code: $FIRST_CI_PUSH_STDOUT"
 echo $FIRST_CI_PUSH_STATUS
   # Could be an error or a success
 if [ $FIRST_CI_PUSH_STATUS -eq 0 ]; then
@@ -15,7 +15,7 @@ if [ $FIRST_CI_PUSH_STATUS -eq 0 ]; then
     RELEASES_PUSH_STDOUT=$(git push origin main:releases --porcelain 2>1)
     RELEASES_PUSH_STATUS=$?
     echo $RELEASES_PUSH_STDOUT
-    echo $RELEASES_PUSH_STATUS
+    echo "==> command exit code: $RELEASES_PUSH_STATUS"
     if  [ $RELEASES_PUSH_STATUS -eq 0 ]; then
       echo '==> Successfully pushed to releases'
       exit 0;
@@ -28,7 +28,7 @@ if [ $FIRST_CI_PUSH_STATUS -eq 0 ]; then
     echo "Successfully pushed to first-ci-build."
     exit 0;
   fi
-else 
+else
   # Error, cannot push, means another branch was faster at pushing, fail silently.
   echo '==> Could not push to "first-ci-passed" branch, probably because CI ran faster on a more recent commit.'
   exit 0;
