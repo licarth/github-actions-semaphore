108b56b1caa11a56c7fda3bca3f4ab90b12e0c7d
Thomas Carli <thomas.carli@gmail.com>
2021-05-12 11:39:46 +0200

    circleci config

--- push.sh
+++ push.sh
@@ -4,16 +4,11 @@
 
 
 FIRST_CI_PUSH_STDOUT=`git push origin main:first-ci-passed --porcelain 2>&1`
-# FIRST_CI_PUSH_STDOUT=`echo $PWD`
 FIRST_CI_PUSH_STATUS=$?
 echo $FIRST_CI_PUSH_STDOUT
 echo $FIRST_CI_PUSH_STATUS
   # Could be an error or a success
-if [ $FIRST_CI_PUSH_STATUS -eq 1 ]; then
-  # Error, cannot push, means another branch was faster at pushing, fail silently.
-  echo '==> Could not push to "first-ci-passed" branch, probably because CI ran faster on a more recent commit.'
-  exit 0;
-else 
+if [ $FIRST_CI_PUSH_STATUS -eq 0 ]; then
   if [[ "$FIRST_CI_PUSH_STDOUT" == *'[up to date]'* ]]; then
     # Other CI already passed
     echo '==> The other CI already pushed, carrying with a push to releases...'
@@ -21,11 +16,11 @@ else
     RELEASES_PUSH_STATUS=$?
     echo $RELEASES_PUSH_STDOUT
     echo $RELEASES_PUSH_STATUS
-    if  [ $RELEASES_PUSH_STATUS -eq 1 ]; then
-      echo '==> Could not push to "releases" branch, probably because CI ran faster on a more recent commit.'
+    if  [ $RELEASES_PUSH_STATUS -eq 0 ]; then
+      echo '==> Successfully pushed to releases'
       exit 0;
     else
-      echo '==> Successfully pushed to releases'
+      echo '==> Could not push to "releases" branch, probably because CI ran faster on a more recent commit.'
       exit 0;
     fi
   else
@@ -33,6 +28,10 @@ else
     echo "Successfully pushed to first-ci-build."
     exit 0;
   fi
+else 
+  # Error, cannot push, means another branch was faster at pushing, fail silently.
+  echo '==> Could not push to "first-ci-passed" branch, probably because CI ran faster on a more recent commit.'
+  exit 0;
 fi
 
 
